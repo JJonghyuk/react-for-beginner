d5a74261c8edc313fa50ac2437b6f7afcc074c25
JJonghyuk <kilerjong@naver.com>
2025-01-22 13:27:41 +0900

    [react-for-beginner] - 연습

--- project-study-info/info.js
+++ project-study-info/info.js
@@ -481,6 +481,25 @@
 // # 7.8
 
 // # 7.9
+// Array.prototype.slice()
+// slice() 메서드는 어떤 배열의 begin부터 end까지(end 미포함)에 대한 얕은 복사본을 새로운 배열 객체로 반환합니다. 원본 배열은 바뀌지 않습니다.
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+
+// String.prototype.substring()
+// (slice외에도 substring이나 substr(비추천)를 사용해서도 문자열을 자를 수도 있습니다.)
+// substring() 메소드는 string 객체의 시작 인덱스부터 종료 인덱스 전 까지 문자열의 부분 문자열을 반환합니다.
+
+// 사용 예시
+// ```
+// const str = 'Mozilla';
+
+// console.log(str.substring(1, 3));
+// // expected output: "oz"
+
+// console.log(str.substring(2));
+// // expected output: "zilla"
+// ```
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/String/substring
 
 // # 7.10
 
