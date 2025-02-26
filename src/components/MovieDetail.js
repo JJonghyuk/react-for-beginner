d4cc73111dde208a86ccb89e85c85b7b744ad3b8
JJonghyuk <kilerjong@naver.com>
2025-02-26 16:37:56 +0900

    리엑트 기본

---
+++ src/components/MovieDetail.js
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+
+function MovieDetail({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+MovieDetail.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default MovieDetail;
